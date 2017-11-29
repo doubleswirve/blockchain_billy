31621a57d2303100934d1fced0c781d863f55b1b
Saxon Stiller <saxon.stiller@gmail.com>
2017-11-28 22:41:34 -0500

    tiny change to reduce sample size while testing

--- models/coins.js
+++ models/coins.js
@@ -4,7 +4,7 @@ module.exports = {
   async getCoins (limit) {
     const response =
       // TODO: Maybe make URL an env var...
-      await fetch(`https://api.coinmarketcap.com/v1/ticker/?limit=${limit || 50}`)
+      await fetch(`https://api.coinmarketcap.com/v1/ticker/?limit=${limit || 20}`)
     const json = response.json()
     return json
   }
