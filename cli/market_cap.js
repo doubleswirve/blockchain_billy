30f3b2a2db2c9d3d55c61ac239064a506dc3d6a4
Saxon Stiller <saxon.stiller@gmail.com>
2017-11-28 23:37:22 -0500

    add roughly formatted display of updated coin market cap

--- cli/market_cap.js
+++ cli/market_cap.js
@@ -1,12 +1,67 @@
 const chalk = require('chalk')
+const columnify = require('columnify')
 const logUpdate = require('log-update')
+const Coins = require('../events/coins')
 
-let i = 0
+const coins = new Coins()
 
-setInterval(() => {
-  logUpdate(
-`
-  billy ${chalk.blue(i++)}
-`
-  )
-}, 1e3)
+function getNameDisplay (name, symbol) {
+  return `${name} (${symbol})`
+}
+
+function getPercentChangeDisplay (percentChange) {
+  const percentDisplayString = `${percentChange}%`
+
+  if (percentChange === 0) {
+    return percentDisplayString
+  }
+
+  if (percentChange < 0) {
+    return chalk.red(percentDisplayString)
+  }
+
+  return chalk.green(percentDisplayString)
+}
+
+function getPriceDisplay (price) {
+  return chalk.bold(`$${price}`)
+}
+
+function getLastUpdatedDisplay (lastUpdated) {
+  const now = parseInt(Date.now() / 1e3)
+  return `${now - lastUpdated}s ago`
+}
+
+coins.on('data', (json) => {
+  const formattedData = json.map((coin) => {
+    const {
+      name,
+      symbol,
+      price_usd,
+      market_cap_usd,
+      percent_change_24h,
+      last_updated
+    } = coin
+    return Object.assign({}, coin, {
+      name: getNameDisplay(name, symbol),
+      price_usd: getPriceDisplay(price_usd),
+      market_cap_usd: getPriceDisplay(market_cap_usd),
+      percent_change_24h: getPercentChangeDisplay(percent_change_24h),
+      last_updated: getLastUpdatedDisplay(last_updated)
+    })
+  })
+  const string = columnify(formattedData, {
+    columns: [
+      'rank',
+      'name',
+      'price_usd',
+      'market_cap_usd',
+      'percent_change_24h',
+      'last_updated'
+    ]
+  })
+  logUpdate(string)
+})
+
+logUpdate('Loading coins...')
+coins.poll()
