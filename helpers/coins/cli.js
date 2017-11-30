1bb62421d2c04a66ec8b050960bb6a1bf3277c35
Saxon Stiller <saxon.stiller@gmail.com>
2017-11-29 23:49:28 -0500

    refactor coins cli display helpers, mostly for style

--- helpers/coins/cli.js
+++ helpers/coins/cli.js
@@ -1,35 +1,43 @@
 const chalk = require('chalk')
 const numeral = require('numeral')
 
-module.exports = {
-  getFullName (name, symbol) {
-    return `${name} (${symbol})`
-  },
-  getLastUpdated (timestamp) {
-    const now = parseInt(Date.now() / 1e3)
-    return `${now - timestamp}s ago`
-  },
-  getPercentChange (percentChange) {
-    const display = `${numeral(percentChange).format('0,0.00')}%`
+function getFullName (name, symbol) {
+  return `${name} (${symbol})`
+}
 
-    if (percentChange === 0) {
-      return display
-    }
+function getLastUpdated (timestamp) {
+  const now = parseInt(Date.now() / 1e3)
+  return `${now - timestamp}s ago`
+}
 
-    if (percentChange < 0) {
-      return chalk.red(display)
-    }
+function getPercentChange (percentChange) {
+  const display = `${numeral(percentChange).format('0,0.00')}%`
 
-    return chalk.green(display)
-  },
-  getPrice (price, whole, unit, bolded) {
-    const formatter = whole === true ? '0,0' : '0,0.00'
-    const display = `${unit || '$'}${numeral(price).format(formatter)}`
-
-    if (bolded === false) {
-      return display
-    }
-
-    return chalk.bold(display)
+  if (percentChange === 0) {
+    return display
   }
+
+  if (percentChange < 0) {
+    return chalk.red(display)
+  }
+
+  return chalk.green(display)
+}
+
+function getPrice (price, whole, unit, bolded) {
+  const formatter = whole === true ? '0,0' : '0,0.00'
+  const display = `${unit || '$'}${numeral(price).format(formatter)}`
+
+  if (bolded === false) {
+    return display
+  }
+
+  return chalk.bold(display)
+}
+
+module.exports = {
+  getFullName,
+  getLastUpdated,
+  getPercentChange,
+  getPrice
 }
