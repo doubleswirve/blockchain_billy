efec05231590e45e8f11fbeb0760c550a1701f31
Saxon Stiller <saxon.stiller@gmail.com>
2017-11-29 22:59:18 -0500

    tiny change to remove var not needed

--- models/coins.js
+++ models/coins.js
@@ -5,7 +5,6 @@ module.exports = {
     const response =
       // TODO: Maybe make URL an env var...
       await fetch(`https://api.coinmarketcap.com/v1/ticker/?limit=${limit || 20}`)
-    const json = response.json()
-    return json
+    return response.json()
   }
 }
