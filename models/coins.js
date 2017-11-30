c0a3ab189f8c4102ca865053c28bc2d4f3939369
Saxon Stiller <saxon.stiller@gmail.com>
2017-11-29 23:43:12 -0500

    refactor coins model a bit, mostly for style

--- models/coins.js
+++ models/coins.js
@@ -1,10 +1,12 @@
 const fetch = require('node-fetch')
 
+async function getCoins (limit) {
+  const response =
+    // TODO: Maybe make URL an env var...
+    await fetch(`https://api.coinmarketcap.com/v1/ticker/?limit=${limit || 100}`)
+  return response.json()
+}
+
 module.exports = {
-  async getCoins (limit) {
-    const response =
-      // TODO: Maybe make URL an env var...
-      await fetch(`https://api.coinmarketcap.com/v1/ticker/?limit=${limit || 100}`)
-    return response.json()
-  }
+  getCoins
 }
