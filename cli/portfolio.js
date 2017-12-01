15e9857576c15242591445fbab6a17b328e3148a
Saxon Stiller <saxon.stiller@gmail.com>
2017-12-01 01:43:11 -0500

    port portfolio demo to blessed as well

--- cli/portfolio.js
+++ cli/portfolio.js
@@ -1,6 +1,6 @@
+const blessed = require('blessed')
 const columnify = require('columnify')
 const fs = require('fs')
-const logUpdate = require('log-update')
 const path = require('path')
 const util = require('util')
 const yaml = require('js-yaml')
@@ -9,6 +9,18 @@ const cliDisplay = require('../helpers/coins/cli')
 const coinDataHelper = require('../helpers/coins/data')
 
 const readFileAsync = util.promisify(fs.readFile)
+
+const screen = blessed.screen()
+const box = blessed.box({
+  alwaysScroll: true,
+  content: 'Loading portfolio...',
+  keys: true,
+  mouse: true,
+  scrollable: true,
+  vi: true
+})
+
+screen.append(box)
 
 async function run () {
   const filename = path.join(__dirname, '../portfolios/sample.yaml')
@@ -37,12 +49,17 @@ async function run () {
       ]
     })
 
-    logUpdate(string)
+    box.setContent(`Last request: ${new Date()}\n\n${string}`)
+    screen.render()
   })
 
   coinData.poll()
 }
 
-logUpdate('Loading portfolio...')
+screen.key([ 'C-c' ], (ch, key) => {
+  return process.exit(0)
+})
+
+screen.render()
 
 run()
