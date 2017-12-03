9ef184368089b9ff1e594b89209eb5c974624d84
Saxon Stiller <saxon.stiller@gmail.com>
2017-12-03 17:19:22 -0500

    sketch out setup for saving local portfolios

--- models/portfolios.js
+++ models/portfolios.js
@@ -1,16 +1,37 @@
 const os = require('os')
 const path = require('path')
+const uuid = require('uuid/v4')
 const yaml = require('js-yaml')
 const fsHelpers = require('../helpers/fs')
+
+async function createLocalDataDirectory () {
+  return fsHelpers.createDirectoryIfDoesNotExist(getLocalDataDirectory())
+}
+
+async function createLocalPortfoliosDirectory () {
+  return fsHelpers.createDirectoryIfDoesNotExist(getLocalPortfoliosDirectory())
+}
+
+function generateLocalPortfolioId () {
+  return uuid()
+}
+
+function getLocalDataDirectory () {
+  return path.join(os.homedir(), '.blockchain_billy')
+}
 
 function getLocalPortfoliosDirectory () {
   // TODO: Make constant for dir name
   // TODO: Maybe allow override via env var
-  return path.join(os.homedir(), '.blockchain_billy', 'portfolios')
+  return path.join(getLocalDataDirectory(), 'portfolios')
 }
 
 function getLocalPortfolioFilePath (portfolioId) {
   return path.join(getLocalPortfoliosDirectory(), `${portfolioId}.yaml`)
+}
+
+function getLocalPortfolioId ({ id }) {
+  return id || generateLocalPortfolioId()
 }
 
 async function getLocalPortfoliosFilePaths () {
@@ -43,7 +64,22 @@ function parseLocalPortfolio (portfolio) {
   return yaml.safeLoad(portfolio)
 }
 
+async function saveLocalPortfolio (portfolio) {
+  await createLocalDataDirectory()
+  await createLocalPortfoliosDirectory()
+  const id = getLocalPortfolioId(portfolio)
+  const portfolioFilePath = getLocalPortfolioFilePath(id)
+  console.log(portfolioFilePath)
+  const data = stringifyLocalPortfolio(Object.assign({}, portfolio, { id }))
+  return fsHelpers.writeFile(portfolioFilePath, data)
+}
+
+function stringifyLocalPortfolio (portfolio) {
+  return yaml.safeDump(portfolio)
+}
+
 module.exports = {
   getLocalPortfolio,
-  getLocalPortfolios
+  getLocalPortfolios,
+  saveLocalPortfolio
 }
